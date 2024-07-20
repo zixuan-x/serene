e689c2b170c26684d0ce91fe3dcb691b8899ed47
Zixuan Zhang <zixuanzhang.x@gmail.com>
2024-07-21 00:17:26 +0100

    Fix CI

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,8 @@
 use server::app;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
     axum::serve(listener, app()).await.unwrap();
+    Ok(())
 }
