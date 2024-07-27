f27515cc0d5dc763f1029f0c5724ce0aee113757
Zixuan Zhang <zixuanzhang.x@gmail.com>
2024-07-27 18:56:11 +0100

    Setup frontend Vite, tailwincss, and NextUI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,12 @@
-import { Button } from "@nextui-org/react";
-import "./App.css";
+import './App.css'
+import { Button } from '@nextui-org/react'
 
 function App() {
   return (
     <>
-      <div
-        style={{
-          width: "30%",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        Menu
-      </div>
-      <div>
-        <Button>Login</Button>
-      </div>
+      <Button>click me</Button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
