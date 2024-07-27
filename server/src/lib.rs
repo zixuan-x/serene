f259d56205e7fe0dd1b8f19d62bb0fc3514a67b9
Zixuan Zhang <zixuanzhang.x@gmail.com>
2024-07-27 01:57:53 +0100

    Add request/response logging

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,11 +1,22 @@
-use axum::{http::StatusCode, response::IntoResponse, Router};
+use axum::Router;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod routes;
 
 pub fn router() -> Router {
-    Router::new().merge(routes::routes()).fallback(handle_404)
-}
-
-async fn handle_404() -> impl IntoResponse {
-    (StatusCode::NOT_FOUND, "Not Found")
+    tracing_subscriber::registry()
+        // .with(
+        //     tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        //         "example_tracing_aka_logging=debug,tower_http=debug,axum::rejection=trace".into()
+        //     }),
+        // )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    Router::new().merge(routes::routes()).layer(
+        ServiceBuilder::new()
+            .layer(TraceLayer::new_for_http())
+            .into_inner(),
+    )
 }
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,6 +1,12 @@
-use axum::Router;
+use axum::{http::StatusCode, response::IntoResponse, Router};
 mod bff;
 
 pub fn routes() -> Router {
-    Router::new().nest("/bff", bff::routes())
+    Router::new()
+        .nest("/bff", bff::routes())
+        .fallback(handle_404)
+}
+
+async fn handle_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Not Found")
 }
